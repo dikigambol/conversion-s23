c0bd1c5aecb5d0d3dabbb118b06398da2df0b937
Diki Akbar <57055952+dikigambol@users.noreply.github.com>
2023-12-19 23:22:36 +0700

    first commit

--- app/Http/Controllers/S23Controller.php
+++ app/Http/Controllers/S23Controller.php
@@ -17,29 +17,29 @@ class S23Controller extends Controller
         }
 
         $zipFilename = time() . '.zip';
-        $zipPath = storage_path("app/public/zip_files/$zipFilename");
-
-        $zip = new ZipArchive();
-        if ($zip->open($zipPath) === TRUE) {
-            $numFiles = $zip->numFiles;
-            for ($i = 0; $i < $numFiles; $i++) {
-                $filename = $zip->getNameIndex($i);
-                echo "File $i: $filename\n";
-            }
-            $zip->close();
-        } else {
-            echo "Gagal membuka arsip ZIP: " . $zip->getStatusString();
-        }
+        $zipPath = public_path("zip_files/$zipFilename");
+        copy($uploadedFile->path(), $zipPath);
 
         $extractPath = public_path('extracted_files');
 
         $zip = new ZipArchive();
+
         if ($zip->open($zipPath) === TRUE) {
-            $zip->extractTo($extractPath);
-            $zip->close();
-            return response()->json(['message' => 'Berhasil Diubah dan Diekstrak.']);
+            if ($zip->extractTo($extractPath)) {
+                $zip->close();
+                // unlink($zipPath);
+                return response()->json(['message' => 'Berhasil Diubah dan Diekstrak.']);
+            } else {
+                $zip->getStatusString();
+                $zip->close();
+                // unlink($zipPath);
+                return response()->json(['message' => "Gagal mengekstrak file"]);
+            }
         } else {
-            return response()->json(['message' => 'Gagal mengekstrak file ZIP.']);
+            $zip->getStatusString();
+            $zip->close();
+            // unlink($zipPath);
+            return response()->json(['message' => "Gagal membuka file zip untuk diekstrak"]);
         }
     }
 }
