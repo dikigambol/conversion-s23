6b0dcc1ce70a4564f3eb91321220d7e9c9e35009
Diki Akbar <57055952+dikigambol@users.noreply.github.com>
2023-12-22 17:00:41 +0700

    updates

--- app/Http/Controllers/S23Controller.php
+++ app/Http/Controllers/S23Controller.php
@@ -27,20 +27,33 @@ class S23Controller extends Controller
         if ($zip->open($zipPath) === TRUE) {
             if ($zip->extractTo($extractPath)) {
                 $zip->close();
-                // unlink($zipPath);
+                unlink($zipPath);
                 return response()->json(['message' => 'Berhasil Diubah dan Diekstrak.']);
             } else {
-                $zip->getStatusString();
                 $zip->close();
-                // unlink($zipPath);
+                unlink($zipPath);
                 return response()->json(['message' => "Gagal mengekstrak file"]);
             }
         } else {
-            $zip->getStatusString();
             $zip->close();
-            // unlink($zipPath);
+            unlink($zipPath);
             return response()->json(['message' => "Gagal membuka file zip untuk diekstrak"]);
         }
+    }
+
+    public function convertS23toZip(Request $request)
+    {
+        $uploadedFile = $request->file('file');
+
+        if ($uploadedFile->getClientOriginalExtension() !== 's23') {
+            return response()->json(['message' => 'File bukan format .s23.']);
+        }
+
+        $zipFilename = time() . '.zip';
+        $zipPath = public_path("zip_files/$zipFilename");
+        copy($uploadedFile->path(), $zipPath);
+
+        return response()->json(['message' => "File Convert to Zip"]);
     }
 
     public function extractS23(Request $request)
@@ -57,6 +70,44 @@ class S23Controller extends Controller
             return "File berhasil diekstrak";
         } else {
             $zip->getStatusString();
+        }
+    }
+
+    public function createZipS23(Request $request)
+    {
+        $s23FilePath = $request->file('file');
+
+        $zip = new ZipArchive();
+
+        if ($zip->open($s23FilePath) === FALSE) {
+            return response()->json(['message' => 'Gagal membuka file .s23.'], 500);
+        }
+
+        $numFiles = $zip->numFiles;
+
+        $fileNames = [];
+
+        for ($i = 0; $i < $numFiles; $i++) {
+            $fileNames[] = $zip->getNameIndex($i);
+        }
+
+        $zip->close();
+
+        return $fileNames;
+    }
+
+    public function unZipFiles(Request $request)
+    {
+        $zipFilePath = $request->file('file');
+        $extractPath = public_path('extracted_files');
+
+        $zip = new ZipArchive();
+
+        if ($zip->open($zipFilePath) === TRUE) {
+            $zip->extractTo($extractPath);
+            $zip->close();
+        } else {
+            echo "File ZIP tidak valid.";
         }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CsrfTokenController;
 use App\Http\Controllers\S23Controller;
 
-Route::post('/converts23', [S23Controller::class, 'convertAndExtract']);
+Route::post('/converts-extract', [S23Controller::class, 'convertAndExtract']);
+Route::post('/converts-zip', [S23Controller::class, 'convertS23toZip']);
 Route::post('/extracts23', [S23Controller::class, 'extractS23']);
+Route::post('/create-zip-s23', [S23Controller::class, 'createZipS23']);
+Route::post('/unzip', [S23Controller::class, 'unZipFiles']);
 Route::get('/get-csrf-token', [CsrfTokenController::class, 'showCsrfToken']);
